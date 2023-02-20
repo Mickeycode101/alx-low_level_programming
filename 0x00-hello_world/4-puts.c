c41491f0639d8e97a08645ec4ca1b58af69c998b
Michael Ofoye <ejiromichael12345@gmail.com>
2023-02-20 10:56:29 -0800

    Hello world put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main -A program that prinys a line using the printf function
- * Return 0 (success)
-*/
-
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle/n");
-	return (0);
+ * main - A program that prints a line using the puts function
+ * Return 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle/n");
+return (0);
 }
+
